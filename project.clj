cbabea4c83504c87aeafe9a3ad2807066749860f
Wilker Lucio <wilkerlucio@gmail.com>
2017-08-13 19:23:55 -0300

    Bump version

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject com.wsscode/pathom "1.0.0-beta3-SNAPSHOT"
+(defproject com.wsscode/pathom "1.0.0-beta4-SNAPSHOT"
   :description "A Clojure library designed to provide a collection of helper functions to support Clojure(script) graph parsers using\nom.next graph syntax."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17" :scope "provided"]
                  [org.clojure/clojurescript "1.9.671" :scope "provided"]
-                 [org.omcljs/om "1.0.0-beta2-SNAPSHOT" :scope "provided"]])
+                 [org.omcljs/om "1.0.0-beta1" :scope "provided"]])
