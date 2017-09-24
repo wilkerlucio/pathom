31532ca6f937817e9de997af2a138820ebfcbe1a
Wilker Lucio <wilkerlucio@gmail.com>
2017-09-24 13:42:16 -0300

    Add some code examples for docs

---
+++ doc-examples/pathom_docs/vector_dispatch.clj
@@ -0,0 +1,13 @@
+(ns com.wsscode.pathom-docs.vector-dispatch
+  (:require [com.wsscode.pathom.core :as p]))
+
+(def name-reader
+  {:name   (fn [_] "Saul")})
+
+(def family-reader
+  {:family (fn [_] "Goodman")})
+
+(def parser (p/parser {::p/plugins [(p/env-plugin {::p/reader [name-reader family-reader]})]}))
+
+(parser {} [{:current-user [:name :family :other]}])
+; => {:current-user {:name "Saul" :family "Goodman" :other ::p/not-found}}
