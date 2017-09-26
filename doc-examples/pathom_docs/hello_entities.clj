69eb87a64acab670db566dc7e15a7b353cdb1e39
Wilker Lucio <wilkerlucio@gmail.com>
2017-09-26 08:16:15 -0300

    Update hello examples

--- doc-examples/pathom_docs/hello_entities.clj
+++ doc-examples/pathom_docs/hello_entities.clj
@@ -1,87 +1,87 @@
 (ns pathom-docs.hello-entities
   (:require [com.wsscode.pathom.core :as p]))
 
-; define some data of plant families
-(def families
-  {:sativa #:family{:name        "Sativa"
-                    :members-ids [:sd]}
-   :hybrid #:family{:name        "Hybrid"
-                    :members-ids [:bd :gsc :ok]}
-   :indica #:family{:name        "Indica"
-                    :members-ids [:bbk :gp]}})
+; define some data of tv shows
+(def tv-shows
+  {:rm  #:tv-show{:title         "Rick and Morty"
+                  :character-ids [:rick :summer :morty]}
+   :bcs #:tv-show{:title         "Better Call Saul"
+                  :character-ids [:bcs]}
+   :got #:tv-show{:title         "Game of Thrones"
+                  :character-ids [:arya :ygritte]}})
 
-; plants data
-(def plants
-  {:bd  #:plant{:name "Blue Dream" :family-id :hybrid}
-   :sd  #:plant{:name "Sour Diesel" :family-id :sativa}
-   :gsc #:plant{:name "Girl Scout Cookies" :family-id :hybrid}
-   :bbk #:plant{:name "Blueberry Kush" :family-id :indica}
-   :ok  #:plant{:name "OG Kush" :family-id :hybrid}
-   :gp  #:plant{:name "Granddaddy Purple" :family-id :indica}})
+; characters data
+(def characters
+  {:rick    #:character{:name "Rick Sanshes" :tv-show-id :rm}
+   :summer  #:character{:name "Summer Smith" :tv-show-id :rm}
+   :saul    #:character{:name "Saul Goodman" :tv-show-id :bcs}
+   :arya    #:character{:name "Arya Stark" :tv-show-id :got}
+   :morty   #:character{:name "Morty Smith" :tv-show-id :rm}
+   :ygritte #:character{:name "Ygritte" :tv-show-id :got}})
 
 ; helper to illustrate what would be a function to your database or
 ; service, it's a good practice to send the entire environment, in this
 ; case we are getting the db, but having the entire env is often empowering
-(defn plants-by-ids [{::keys [db]} ids]
-  (map (get @db :plants) ids))
+(defn characters-by-ids [{::keys [db]} ids]
+  (map (get @db :characters) ids))
 
 (def computed
-  ; example of a global attribute, a random a random plant from our
+  ; example of a global attribute, a random character from our
   ; "database" that can be fetched at any time
-  {:plants/random
+  {:characters/random
    ; pretend the db is your datomic database or a Postgres connection,
    ; anything that would enable you to reach the data
    (fn [{::keys [db] :as env}]
      ; take a hand of the entity we want to be the current node
-     (let [plant (rand-nth (-> @db :plants vals vec))]
+     (let [character (rand-nth (-> @db :characters vals vec))]
        ; to parse the sub-query with the entity we use the join function
-       (p/join plant env)))
+       (p/join character env)))
 
    ; example when you want to do go down the parser with a list of things
-   ; very much like the single one, but using join-seq instead
-   :plants/popular
+   ; very much like the single one, but using join-seq instead, in this case
+   ; the main characters that we have on our list
+   :characters/main
    (fn [env]
-     ; since we decided to get the env in the plants-by-ids the argument
+     ; since we decided to get the env in the characters-by-ids the argument
      ; passing is a brease
-     (p/join-seq env (plants-by-ids env [:bd :sd :gsc :ok :gp])))
+     (p/join-seq env (characters-by-ids env [:rick :morty :saul :arya])))
 
-   ; an example of relashionship, extract the family according to the :plant/family
-   ; on the plant entity
-   :plant/family
+   ; an example of relashionship, extract the tv-show according to the :character/tv-show-id
+   ; on the character entity
+   :character/tv-show
    (fn [{::keys [db] :as env}]
-     ; the p/entity-attr! will try to get the :plant/family from current entity
+     ; the p/entity-attr! will try to get the :character/tv-show from current entity
      ; if it's not there it will make a query for it using the same parser. If
      ; it can't be got it will trigger an exception with the issue details, making
      ; easier to identify the problem
-     (let [family-id (p/entity-attr! env :plant/family-id)]
-       (p/join (some-> @db :families (get family-id)) env)))
+     (let [tv-show-id (p/entity-attr! env :character/tv-show-id)]
+       (p/join (some-> @db :tv-shows (get tv-show-id)) env)))
 
    ; example of making a computed property, this will get the number of
-   ; plants in the current family
-   :family/members-count
+   ; characters in the current tv-show
+   :tv-show/characters-count
    (fn [env]
      ; just give a count on members, and again, will raise exception if
-     ; :family/members fails to be reached
-     (count (p/entity-attr! env :family/members-ids)))})
+     ; :tv-show/character-ids fails to be reached
+     (count (p/entity-attr! env :tv-show/character-ids)))})
 
 (def parser
   ; This time we are using the env-plugin to initialize the environment, this is good
   ; to set the defaults for your parser to be called. Also, we are attaching the built-in
   ; reader map-reader on the game, so it will read the keys from the entity map. Check
-  ; Entity page for more information.
+  ; Entity page on wiki for more information.
   (p/parser {::p/plugins [(p/env-plugin {::p/reader [p/map-reader computed]})]}))
 
 ; call the parser, create and send our atom database
-(parser {::db (atom {:plants   plants
-                     :families families})}
-        [{:plants/popular [:plant/name {:plant/family [:family/name
-                                                       :family/members-count]}]}
+(parser {::db (atom {:characters characters
+                     :tv-shows   tv-shows})}
+        [{:characters/main [:character/name {:character/tv-show [:tv-show/title
+                                                                 :tv-show/characters-count]}]}
          ; feeling lucky today?
-         {:plants/random [:plant/name]}])
+         {:characters/random [:character/name]}])
 ; =>
-; #:plants{:popular [#:plant{:name "Blue Dream", :family #:family{:name "Hybrid", :members-count 3}}
-;                    #:plant{:name "Sour Diesel", :family #:family{:name "Sativa", :members-count 1}}
-;                    #:plant{:name "Girl Scout Cookies", :family #:family{:name "Hybrid", :members-count 3}}
-;                    #:plant{:name "OG Kush", :family #:family{:name "Hybrid", :members-count 3}}
-;                    #:plant{:name "Granddaddy Purple", :family #:family{:name "Indica", :members-count 2}}],
-;          :random #:plant{:name "OG Kush"}}
+; #:characters{:main   [#:character{:name "Rick Sanshes", :tv-show #:tv-show{:name "Rick and Morty", :characters-count 3}}
+;                       #:character{:name "Morty Smith", :tv-show #:tv-show{:name "Rick and Morty", :characters-count 3}}
+;                       #:character{:name "Saul Goodman", :tv-show #:tv-show{:name "Better Call Saul", :characters-count 1}}
+;                       #:character{:name "Arya Stark", :tv-show #:tv-show{:name "Game of Thrones", :characters-count 2}}],
+;              :random #:character{:name "Saul Goodman"}}
--- doc-examples/pathom_docs/hello_entities_entity.clj
+++ doc-examples/pathom_docs/hello_entities_entity.clj
@@ -1,74 +1,75 @@
 (ns pathom-docs.hello-entities
   (:require [com.wsscode.pathom.core :as p]))
 
-; define some data of plant families
-(def families
-  {:sativa #:family{:name        "Sativa"
-                    :members-ids [:sd]}
-   :hybrid #:family{:name        "Hybrid"
-                    :members-ids [:bd :gsc :ok]}
-   :indica #:family{:name        "Indica"
-                    :members-ids [:bbk :gp]}})
+; define some data of tv shows
+(def tv-shows
+  {:rm  #:tv-show{:title         "Rick and Morty"
+                  :character-ids [:rick :summer :morty]}
+   :bcs #:tv-show{:title         "Better Call Saul"
+                  :character-ids [:bcs]}
+   :got #:tv-show{:title         "Game of Thrones"
+                  :character-ids [:arya :ygritte]}})
 
-; plants data
-(def plants
-  {:bd  #:plant{:name "Blue Dream" :family-id :hybrid}
-   :sd  #:plant{:name "Sour Diesel" :family-id :sativa}
-   :gsc #:plant{:name "Girl Scout Cookies" :family-id :hybrid}
-   :bbk #:plant{:name "Blueberry Kush" :family-id :indica}
-   :ok  #:plant{:name "OG Kush" :family-id :hybrid}
-   :gp  #:plant{:name "Granddaddy Purple" :family-id :indica}})
+; characters data
+(def characters
+  {:rick    #:character{:name "Rick Sanshes" :tv-show-id :rm}
+   :summer  #:character{:name "Summer Smith" :tv-show-id :rm}
+   :saul    #:character{:name "Saul Goodman" :tv-show-id :bcs}
+   :arya    #:character{:name "Arya Stark" :tv-show-id :got}
+   :morty   #:character{:name "Morty Smith" :tv-show-id :rm}
+   :ygritte #:character{:name "Ygritte" :tv-show-id :got}})
 
 ; helper to illustrate what would be a function to your database or
 ; service, it's a good practice to send the entire environment, in this
 ; case we are getting the db, but having the entire env is often empowering
-(defn plants-by-ids [{::keys [db]} ids]
-  (map (get @db :plants) ids))
+(defn characters-by-ids [{::keys [db]} ids]
+  (map (get @db :characters) ids))
 
 (def computed
-  ; example of a global attribute, a random a random plant from our
+  ; example of a global attribute, a random character from our
   ; "database" that can be fetched at any time
-  {:plants/random
+  {:characters/random
    ; pretend the db is your datomic database or a Postgres connection,
    ; anything that would enable you to reach the data
    (fn [{::keys [db] :as env}]
      ; take a hand of the entity we want to be the current node
-     (let [plant (rand-nth (-> @db :plants vals vec))]
+     (let [character (rand-nth (-> @db :characters vals vec))]
        ; to parse the sub-query with the entity we use the join function
-       (p/join plant env)))
+       (p/join character env)))
 
    ; example when you want to do go down the parser with a list of things
-   ; very much like the single one, but using join-seq instead
-   :plants/popular
+   ; very much like the single one, but using join-seq instead, in this case
+   ; the main characters that we have on our list
+   :characters/main
    (fn [env]
-     ; since we decided to get the env in the plants-by-ids the argument
+     ; since we decided to get the env in the characters-by-ids the argument
      ; passing is a brease
-     (p/join-seq env (plants-by-ids env [:bd :sd :gsc :ok :gp])))
+     (p/join-seq env (characters-by-ids env [:rick :morty :saul :arya])))
 
-   ; an example of relashionship, extract the family according to the :plant/family
-   ; on the plant entity
-   :plant/family
+   ; an example of relashionship, extract the tv-show according to the :character/tv-show-id
+   ; on the character entity
+   :character/tv-show
    (fn [{::keys [db] :as env}]
-     ; the p/entity-attr! will try to get the :plant/family from current entity
+     ; the p/entity-attr! will try to get the :character/tv-show from current entity
      ; if it's not there it will make a query for it using the same parser. If
      ; it can't be got it will trigger an exception with the issue details, making
      ; easier to identify the problem
-     (let [family-id (p/entity-attr! env :plant/family-id)]
-       (p/join (some-> @db :families (get family-id)) env)))
+     (let [tv-show-id (p/entity-attr! env :character/tv-show-id)]
+       (p/join (some-> @db :tv-shows (get tv-show-id)) env)))
 
-   ; gettings the members from a given family
-   :family/members
+   ; load show characters
+   :tv-show/characters
    (fn [env]
-     (let [member-ids (p/entity-attr! env :family/members-ids)]
-       (p/join-seq env (plants-by-ids env member-ids))))
+     (let [ids (p/entity-attr! env :tv-show/character-ids)]
+       (p/join-seq env (characters-by-ids env ids))))
 
    ; example of making a computed property, this will get the number of
-   ; plants in the current family
-   :family/members-count
+   ; characters in the current tv-show
+   :tv-show/characters-count
    (fn [env]
      ; just give a count on members, and again, will raise exception if
-     ; :family/members fails to be reached
-     (count (p/entity-attr! env :family/members-ids)))})
+     ; :tv-show/character-ids fails to be reached
+     (count (p/entity-attr! env :tv-show/character-ids)))})
 
 ; initialize a multi-method to handle entity queries
 (defmulti entity p/entity-dispatch)
@@ -77,17 +78,20 @@
 ; this reader can't handle the given entry
 (defmethod entity :default [_] ::p/continue)
 
-; let's handle the load of plants by id
-(defmethod entity :plant/id [{::keys [db] :as env}]
-  ; from the key [:plant/id :bbk], p/ident-value will return :bbk
+; let's handle the load of characters by id
+(defmethod entity :character/id [{::keys [db] :as env}]
+  ; from the key [:character/id :saul], p/ident-value will return :saul
   (let [id (p/ident-value env)]
     ; same thing as would find a record by id on your database
-    (p/join (get-in @db [:plants id] ::p/not-found) env)))
+    ; we return ::p/continue to signal this reader wans't able to
+    ; fetch it entity, so the parser can try the next one, more about this
+    ; on Readers with page
+    (p/join (get-in @db [:characters id] ::p/continue) env)))
 
-; same thing for families
-(defmethod entity :family/id [{::keys [db] :as env}]
+; same thing for tv shows
+(defmethod entity :tv-show/id [{::keys [db] :as env}]
   (let [id (p/ident-value env)]
-    (p/join (get-in @db [:families id] ::p/not-found) env)))
+    (p/join (get-in @db [:tv-shows id] ::p/continue) env)))
 
 (def parser
   ; add our entity reader to our reader list
@@ -96,13 +100,15 @@
                                                      entity]})]}))
 
 ; testing our new queries
-(parser {::db (atom {:plants   plants
-                     :families families})}
-        [[:plant/id :bbk]
-         {[:family/id :hybrid]
-          [{:family/members [:plant/name]}]}])
+(parser {::db (atom {:characters characters
+                     :tv-shows   tv-shows})}
+        [[:character/id :arya]
+         {[:tv-show/id :rm]
+          [:tv-show/title
+           {:tv-show/characters [:character/name]}]}])
 ; =>
-; {[:plant/id :bbk] #:plant{:name "Blueberry Kush", :family-id :indica}
-;  [:family/id :hybrid] #:family{:members [#:plant{:name "Blue Dream"}
-;                                          #:plant{:name "Girl Scout Cookies"}
-;                                          #:plant{:name "OG Kush"}]}}
+; {[:character/id :arya] #:character{:name "Arya Stark", :tv-show-id :got}
+;  [:tv-show/id :rm]     #:tv-show{:title      "Rick and Morty"
+;                                  :characters [#:character{:name "Rick Sanshes"}
+;                                               #:character{:name "Summer Smith"}
+;                                               #:character{:name "Morty Smith"}]}}
