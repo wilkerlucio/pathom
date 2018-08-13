fb78728b40fbc4eefeac6e497840d8da6a04aa6a
Wilker Lucio <wilkerlucio@gmail.com>
2018-08-13 10:43:36 -0300

    Bump next

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.wsscode/pathom "2.1.1"
+(defproject com.wsscode/pathom "2.1.2-SNAPSHOT"
   :description "A Clojure library designed to provide a collection of helper functions to support Clojure(script) graph parsers using\nom.next graph syntax."
   :url "https://github.com/wilkerlucio/pathom"
   :license {:name "Eclipse Public License"
