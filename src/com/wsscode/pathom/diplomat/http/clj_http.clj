a23c3e708932223823fa9ac97d2b4c7fdbdd8f58
Enzzo Cavallo <souenzzo@gmail.com>
2018-12-08 00:42:20 -0200

    Now clj-http driver works with github-start example

--- src/com/wsscode/pathom/diplomat/http/clj_http.clj
+++ src/com/wsscode/pathom/diplomat/http/clj_http.clj
@@ -1,7 +1,8 @@
 (ns com.wsscode.pathom.diplomat.http.clj-http
   (:require [clj-http.client :as client]
             [com.wsscode.pathom.diplomat.http :as http]
-            [clojure.spec.alpha :as s]))
+            [clojure.spec.alpha :as s]
+            [com.wsscode.common.async-clj :refer [go-catch]]))
 
 ;; TODO: ::http/debug?
 (defn build-request-map
@@ -10,17 +11,30 @@
     (cond-> {:url    url
              :method (keyword (http/request-method req))}
             (q? ::http/headers) (assoc :headers headers)
-            (q? ::http/content-type) (assoc :content-type (http/encode-type->header content-type))
+            (q? ::http/content-type) (assoc :content-type (keyword (name content-type)))
             (q? ::http/accept) (assoc :accept (http/encode-type->header accept))
-            (q? ::http/as) (assoc :as (http/encode-type->header as))
+            (q? ::http/as) (assoc :as (keyword (name as)))
             (q? ::http/form-params) (assoc :form-params form-params)
             (q? ::http/body) (assoc :body body))))
+
+(defn build-response-map
+  [{:keys [status body headers]
+    :as   response}]
+  (with-meta {::http/status  status
+              ::http/body    body
+              ::http/headers headers}
+             response))
 
 (defn request [req]
   (s/assert ::http/request req)
   (-> req
       build-request-map
-      client/request))
+      client/request
+      build-response-map))
+
+(defn request-async
+  [req]
+  (go-catch (request req)))
 
 (s/fdef request
   :args (s/cat :request ::http/request)
