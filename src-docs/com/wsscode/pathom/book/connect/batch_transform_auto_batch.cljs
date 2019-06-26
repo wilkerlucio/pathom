5ca4558f167a6a45550dd0da1048ebca70f3b105
Wilker Lucio <wilkerlucio@gmail.com>
2019-06-26 10:19:56 -0300

    Add docs on transform-auto-batch

---
+++ src-docs/com/wsscode/pathom/book/connect/batch_transform_auto_batch.cljs
@@ -0,0 +1,36 @@
+(ns com.wsscode.pathom.book.connect.batch-transform-auto-batch
+  (:require [com.wsscode.pathom.core :as p]
+            [com.wsscode.pathom.connect :as pc]
+            [cljs.core.async :as async :refer [go]]))
+
+(pc/defresolver list-things [_ _]
+  {::pc/output [{:items [:number]}]}
+  {:items [{:number 3}
+           {:number 10}
+           {:number 18}]})
+
+(pc/defresolver slow-resolver [_ {:keys [number]}]
+  {::pc/input     #{:number}
+   ::pc/output    [:number-added]
+   ; set auto-batch with concurrency of 10
+   ::pc/transform (pc/transform-auto-batch 10)}
+  (go
+    (async/<! (async/timeout 1000))
+    ; dealing with the single case, as in the first example we did on batch
+    {:number-added (inc number)}))
+
+(def app-registry [list-things slow-resolver])
+
+(def parser
+  (p/async-parser
+    {::p/env     {::p/reader        [p/map-reader
+                                     pc/async-reader2
+                                     pc/open-ident-reader]
+                  ::p/process-error (fn [env error]
+                                      (js/console.error "ERROR" error)
+                                      (p/error-str error))}
+     ::p/mutate  pc/mutate-async
+     ::p/plugins [(pc/connect-plugin {::pc/register app-registry})
+                  p/error-handler-plugin
+                  p/request-cache-plugin
+                  p/trace-plugin]}))
--- src-docs/com/wsscode/pathom/book/interactive_parser.cljs
+++ src-docs/com/wsscode/pathom/book/interactive_parser.cljs
@@ -8,6 +8,7 @@
             [com.wsscode.pathom.book.connect.batch]
             [com.wsscode.pathom.book.connect.batch2]
             [com.wsscode.pathom.book.connect.batch3]
+            [com.wsscode.pathom.book.connect.batch-transform-auto-batch]
             [com.wsscode.pathom.book.connect.getting-started]
             [com.wsscode.pathom.book.connect.getting-started2]
             [com.wsscode.pathom.book.connect.mutation-async]
@@ -31,6 +32,7 @@
    "connect.batch"                 {::parser com.wsscode.pathom.book.connect.batch/parser}
    "connect.batch2"                {::parser com.wsscode.pathom.book.connect.batch2/parser}
    "connect.batch3"                {::parser com.wsscode.pathom.book.connect.batch3/parser}
+   "connect.transform-auto-batch"  {::parser com.wsscode.pathom.book.connect.batch-transform-auto-batch/parser}
    "connect.getting-started"       {::parser com.wsscode.pathom.book.connect.getting-started/parser
                                     ::ns     "com.wsscode.pathom.book.connect.getting-started"}
    "connect.getting-started2"      {::parser com.wsscode.pathom.book.connect.getting-started2/parser
