aaffaf0a87856412ce8a751e156aa0dd21ba019b
Wilker Lucio <wilkerlucio@gmail.com>
2019-09-15 23:37:50 -0300

    Extract sugar pathom constructors to sugar namespace

--- src/com/wsscode/pathom/connect/datomic.clj
+++ src/com/wsscode/pathom/connect/datomic.clj
@@ -3,7 +3,7 @@
             [clojure.spec.alpha :as s]
             [com.wsscode.pathom.connect :as pc]
             [com.wsscode.pathom.core :as p]
-            [com.wsscode.pathom.misc :as misc]
+            [com.wsscode.pathom.sugar :as ps]
             [datomic.api :as d]
             [edn-query-language.core :as eql]))
 
@@ -208,44 +208,13 @@
             {:db/id (zipmap (map hash-set (schema->uniques schema)) (repeat #{resolver}))}
             (vals schema)))}))
 
-(defn connect-serial-parser
-  "Create a standard connect parser using the serial parser.
-
-  This parser recommended for handling small and simple queries, like
-  resolvers to process missing configuration options."
-  [register]
-  (p/parser
-    {::p/env     {::p/reader               [p/map-reader
-                                            pc/reader2
-                                            pc/open-ident-reader
-                                            p/env-placeholder-reader]
-                  ::p/placeholder-prefixes #{">"}}
-     ::p/mutate  pc/mutate
-     ::p/plugins [(pc/connect-plugin {::pc/register register})
-                  p/error-handler-plugin
-                  p/trace-plugin]}))
-
-(defn context-parser
-  "Transforms the signature of a regular parser to one that takes
-  some initial context to run the query. This returns a fn with
-  the following arities:
-
-  [context query] => runs query using context as initial data.
-  [env context query] => same as before but accepts initial environment."
-  [parser]
-  (fn context-parser-internal
-    ([context query]
-     (parser {::p/entity (atom context)} query))
-    ([env context query]
-     (parser (assoc env ::p/entity (atom context)) query))))
-
 (def registry
   [conn->db-resolver
    db->schema-resolver
    schema->schema-keys-resolver
    schema->uniques-resolver])
 
-(def config-parser (-> registry connect-serial-parser context-parser))
+(def config-parser (-> registry ps/connect-serial-parser ps/context-parser))
 
 (defn normalize-config [config]
   (config-parser config [::conn ::db ::schema ::schema-uniques ::schema-keys]))
