58baf32c82b92d382b46b11497c9ba4d975b8fad
Szabó Krisztián <thenonameguy24@gmail.com>
2020-03-05 20:25:03 +0100

    Update leiningen deps, remove unused ones

--- project.clj
+++ project.clj
@@ -7,11 +7,8 @@
   :source-paths ["src"]
 
   :dependencies [[camel-snake-kebab "0.4.0"]
-                 [com.wsscode/spec-inspec "1.0.0-alpha2"]
-                 [org.clojure/data.json "0.2.6"]
-                 [spec-coerce "1.0.0-alpha6"]
                  [edn-query-language/eql "0.0.9"]
-                 [org.clojure/core.async "0.4.474"]
+                 [org.clojure/core.async "0.4.490"]
                  [org.clojure/test.check "0.10.0-alpha3"]
 
                  [com.cognitect/transit-clj "0.8.313" :scope "test"]
