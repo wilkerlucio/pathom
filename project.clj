8d22d6de2b6f6eb35fb82a2e18582ccb6b6ef502
Wilker Lucio <wilkerlucio@gmail.com>
2020-04-02 11:45:14 -0300

    Deps upgrades

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.wsscode/pathom "2.3.0-alpha5"
+(defproject com.wsscode/pathom "2.3.0-alpha6-SNAPSHOT"
   :description "A Clojure library designed to provide a collection of helper functions to support Clojure(script) graph parsers using\nom.next graph syntax."
   :url "https://github.com/wilkerlucio/pathom"
   :license {:name "MIT"
@@ -9,13 +9,12 @@
   :dependencies [[camel-snake-kebab "0.4.0"]
                  [com.wsscode/async "1.0.2"]
                  [com.wsscode/spec-inspec "1.0.0-alpha2"]
-                 [org.clojure/data.json "0.2.6"]
-                 [spec-coerce "1.0.0-alpha6"]
                  [edn-query-language/eql "0.0.9"]
                  [org.clojure/core.async "0.4.474"]
+                 [org.clojure/data.json "0.2.6"]
                  [org.clojure/test.check "0.10.0-alpha3"]
 
-                 [com.cognitect/transit-clj "0.8.313" :scope "test"]
+                 [com.cognitect/transit-clj "1.0.324" :scope "test"]
                  [com.cognitect/transit-cljs "0.8.256" :scope "test"]
 
                  [com.fulcrologic/guardrails "0.0.12"]
